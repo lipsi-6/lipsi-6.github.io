7dd19edf369908def9df95110da0f957bd80a910
lipsi-6 <3zhml.z2mo@gmail.com>
2024-07-06 22:09:38 +0800

    calculator

---
+++ _posts/2024-06-30-calculator.md
@@ -0,0 +1,8 @@
+---
+title: 简易计算器
+date: 2024-06-30 22:04:00 +0800
+catagories: [计算机, JavaScript 课]
+author: lipsi
+---
+
+{% include calc.html %}
---
+++ assets/js/calc.js
@@ -0,0 +1,57 @@
+const expression = document.querySelector(".output");
+let ops = document.querySelectorAll(".op");
+let nums = document.querySelectorAll(".num");
+const del = document.querySelector("#del");
+const equal = document.querySelector("#equal");
+const copy = document.querySelector("#copy");
+let flag = 0;
+
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+      .then(function() {
+        console.log('Text copied to clipboard');
+      })
+      .catch(function(err) {
+        console.error('Failed to copy text: ', err);
+      });
+}
+
+function removeLastCharacter(str) {
+    return str.slice(0, -1);
+}
+
+expression.onclick = function() {
+    expression.textContent = removeLastCharacter(expression.textContent);
+}
+
+del.onclick = function() {
+    expression.textContent = "";
+    flag = 0;
+}
+
+equal.onclick = function() {
+    expression.textContent = eval(expression.textContent);
+    flag = 1;
+}
+
+ops.forEach(op => {
+    op.onclick = function() {
+        flag = 0;
+        expression.textContent += op.textContent;
+    };
+});
+
+nums.forEach(num => {
+    num.onclick = function() {
+        if(flag === 1) {
+            expression.textContent = "";
+            flag = 0;
+        }
+        expression.textContent += num.textContent;
+    };
+});
+
+copy.onclick = function() {
+    copyToClipboard(expression.textContent);
+}
